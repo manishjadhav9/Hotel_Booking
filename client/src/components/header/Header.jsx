7f8e279085062cbbfd5be9ce6aa4f19ef95e9ea5
Manish Jadhav <manishsj289@gmail.com>
2024-09-03 00:46:03 +0530

    basic changes

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -83,7 +83,7 @@ const Header = ({ type }) => {
             </h1>
             <p className="headerDesc">
               Get rewarded for your travels – unlock instant savings of 10% or
-              more with a free Lamabooking account
+              more with a free BookMyStay Account!!!
             </p>
             <button className="headerBtn">Sign in / Register</button>
             <div className="headerSearch">
