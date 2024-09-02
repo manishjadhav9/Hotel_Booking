c854551aac9b795d43939071dcd449c33cf57a7d
Manish Jadhav <manishsj289@gmail.com>
2024-09-02 14:49:17 +0530

    hotel name updated

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2024 La Maison Blanche.</div>
+      <div className="fText">Copyright © 2024 Book My Stay.</div>
     </div>
   );
 };
